3edab1f52cdf3d6019d68fa9eca5f9092ae9dcd7
Forgman75 <agolikov2020@gmail.com>
2026-09-02 20:24:01 +0300

    I added the __add__ magic method to the Product class

--- src/products.py
+++ src/products.py
@@ -64,3 +64,10 @@ class Product:
         """Строковое отображение продукта."""
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
+    def __add__(self, other):
+        """Магический метод сложения. Возвращает сумму произведений цены на количество."""
+        if isinstance(other, Product):
+            return (self.price * self.quantity) + (other.price * other.quantity)
+        raise TypeError("Складывать можно только объекты класса Product")
+
+
