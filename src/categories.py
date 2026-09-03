6a74126dec6e8229453195951fc7fe516d267ed0
Forgman75 <agolikov2020@gmail.com>
2026-09-04 02:23:30 +0300

    Added the CategoryIterator class

--- src/categories.py
+++ src/categories.py
@@ -55,3 +55,33 @@ class Category:
         Рассчитывает общее количество товаров на складе (сумма quantity)."""
         total_quantity = sum(product.quantity for product in self.__products)
         return f"{self.name}, количество продуктов: {total_quantity} шт."
+
+
+class CategoryIterator:
+    """Итератор для перебора товаров одной категории."""
+
+    def __init__(self, category):
+        """
+        Принимает объект категории и инициализирует итератор.
+        
+        :param category: объект класса Category
+        """
+        self._category = category
+        self._products = category.product_list  # список товаров категории
+        self._index = 0                      # текущая позиция в списке
+
+    def __iter__(self):
+        """Возвращает сам объект итератора."""
+        return self
+
+    def __next__(self):
+        """
+        Возвращает очередной товар категории.
+        Когда товары закончатся — возбуждает StopIteration.
+        """
+        if self._index < len(self._products):
+            product = self._products[self._index]
+            self._index += 1
+            return product
+        else:
+            raise StopIteration
