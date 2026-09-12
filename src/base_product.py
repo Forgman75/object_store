b9cb346d8f234971eafe5549c0a58c3a6e732f44
Forgman75 <agolikov2020@gmail.com>
2026-09-12 18:44:50 +0300

    I added an abstract class BaseProduct, from which Product inherits

---
+++ src/base_product.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    """
+    Абстрактный базовый класс
+    """
+    
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            color: str="",
+            *args,
+            **kwargs,
+            ):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+        self.color = color
+
+    @property
+    @abstractmethod
+    def price(self):
+        pass
+
+    @price.setter
+    @abstractmethod
+    def price(self, value):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
