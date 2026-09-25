32d61138b5360de31bdbeba4de90927279b52738
Forgman75 <agolikov2020@gmail.com>
2026-09-26 00:41:17 +0300

    I fixed the ProductZeroQuantityError class

--- src/base_product.py
+++ src/base_product.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 
-class ProductZeroQuantityError(Exception):
+class ProductZeroQuantityError(ValueError):
     """Исключение, вызываемое при попытке добавить товар с нулевым количеством."""
 
     def __init__(self, message="Товар с нулевым количеством не может быть добавлен"):
